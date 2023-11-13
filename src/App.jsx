fa842bf9a59e2896bdc0254eacff27968c5bc7a9
fast2run <127115195+fast2run@users.noreply.github.com>
2023-11-13 18:41:53 +0530

    Add Existing Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,101 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [formData,setFormData]=useState([
+    {
+      id:"1",
+    fname:'',
+    lname:'',
+    age:'',
+    gender:'',
+    mobile:'',
+    email:'',
+    password:'',
+  }]
+)
+  
+  const [theme,setTheme]=useState(false)
+
+  const handleThemeChange=()=> setTheme(!theme)
+
+  useEffect(()=>{
+    if(theme==true){
+      document.body.classList.add("dark")
+    }else{
+      document.body.classList.remove("dark")
+    }
+  })
+  
+
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log(formData)
+  }
+  
+  const handleChange=(e)=>{
+    const {name,value}=e.target;
+    setFormData((prev)=>{
+      return{...prev,[name]:value}
+    })
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <section>
+      <h1>Data Collection from User</h1>
+      <button onClick={handleThemeChange}>{theme?"Dark":"Light"}</button>
+      <hr />
+    </section>
+    <section>
+      <form onSubmit={handleSubmit}>
+        <div className="inputField">
+          <label htmlFor="fname">First Name</label>
+          <input type="text" name="fname"value={formData.fname} onChange={handleChange}/>
+          
+        </div>
+        <div className="inputField">
+          <label htmlFor="lname">Last Name</label>
+          <input type="text" name="lname" value={formData.lname} onChange={handleChange}/>
+        </div>
+        <div className="inputField">
+          <label htmlFor="age">Age</label>
+          <input type="text" name="age" value={formData.age}onChange={handleChange}/>
+        </div>
+        <div className="inputField">
+          <label htmlFor="gender">Gender</label>
+          <input type="text" name="gender" value={formData.gender} onChange={handleChange}/>
+        </div>
+        <div className="inputField">
+          <label htmlFor="mobile">First Name</label>
+          <input type="text" name="mobile" value={formData.mobile} onChange={handleChange}/>
+        </div>
+        <div className="inputField">
+          <label htmlFor="email">First Name</label>
+          <input type="email" name="email" value= {formData.email} onChange={handleChange}/>
+        </div>
+        <div className="inputField">
+          <label htmlFor="password">First Name</label>
+          <input type="password" name="password" value={formData.password} onChange={handleChange}/>
+        </div>
+        <div className="inputField">
+          <button type='submit'>Update Data</button>
+        </div>
+      </form>
+    </section>
+    <section>
+{
+  formData.map((data)=>(
+    <div key={data.id}>
+      <p>{data}</p>
+    </div>
+  ))
+}
+    
+      </section>
+    
     </>
   )
 }
